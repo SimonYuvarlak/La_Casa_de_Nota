6ce1a6f4f0d2fadfdcea698b3fc26f8019111953
Simon Arda Yuvarlak <simonyuvarlak@icloud.com>
2021-10-20 22:47:58 +0300

    make the notes editable

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,18 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import DeleteIcon from "@material-ui/icons/Delete";
 
 function Note(props) {
+  const [note, setNote] = React.useState({
+    title: props.title,
+    content: props.content
+  });
+
+  function update(event){
+    const { name, value } = event.target;
+
+    setNote((prevNote) => {
+      return {
+        ...prevNote,
+        [name]: value
+      };
+    });
+  }
+
   function handleClick() {
     props.onDelete(props.id);
   }
 
   return (
     <div className="note">
-      <h1>{props.title}</h1>
-      <p>{props.content}</p>
-      <button onClick={handleClick}>
-        <DeleteIcon />
-      </button>
+        <input
+            name="title"
+            onChange={update}
+            value={note.title}
+            placeholder="Title"
+        />
+        <textarea
+          name="content"
+          onChange={update}
+          value={note.content}
+          placeholder="Take a note..."
+          rows={5}
+        />
+        <button onClick={handleClick}>
+          <DeleteIcon />
+        </button>
+        <button>
+          Update Note
+        </button>
     </div>
   );
 }
