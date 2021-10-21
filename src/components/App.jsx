cace9a239bcb12b57954707e952e13af155a68e7
EmreKeskin47 <43786018+EmreKeskin47@users.noreply.github.com>
2021-10-21 18:02:39 +0300

    beynine

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,58 +3,57 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
-import {ThemeProvider} from "styled-components";
+import { ThemeProvider } from "styled-components";
 import { GlobalStyles } from "./globalStyles";
 import { lightTheme, darkTheme } from "./Themes";
 
 function App() {
-  const [notes, setNotes] = useState([]);
+    const [notes, setNotes] = useState([]);
 
-  function addNote(newNote) {
-    setNotes(prevNotes => {
-      return [...prevNotes, newNote];
-    });
-  }
+    function addNote(newNote) {
+        setNotes((prevNotes) => {
+            return [...prevNotes, newNote];
+        });
+    }
 
-  function deleteNote(id) {
-    console.log(id + " has been clicked");
-    setNotes(prevNotes => {
-      return prevNotes.filter((noteItem, index) => {
-        return index !== id;
-      });
-    });
-    console.log(notes);
-  }
+    function deleteNote(id) {
+        console.log(id + " has been clicked");
+        notes.filter((index) => {
+            return index !== id;
+        });
+        setNotes([...notes]);
+        console.log(notes);
+    }
 
-  function updateNote(note, id){
-    console.log(notes[id]);
-    setNotes(() => {
-      notes[id].title = note.title;
-      notes[id].content = note.content;
-      return notes;
-    });
-    console.log(notes[id]);
-  }
+    function updateNote(id, name, value) {
+        if (name && name == "title") {
+            notes[id].title = value;
+        } else if (name && name == "content") {
+            notes[id].content = value;
+        }
+        setNotes([...notes]);
+        console.log(notes);
+    }
 
-  return (
+    return (
         <div>
-          <Header />
-          <CreateArea onAdd={addNote} />
-          {notes.map((noteItem, index) => {
-            return (
-              <Note
-                key={index}
-                id={index}
-                title={noteItem.title}
-                content={noteItem.content}
-                onDelete={deleteNote}
-                update={updateNote}
-              />
-            );
-          })}
-          <Footer />
+            <Header />
+            <CreateArea onAdd={addNote} />
+            {notes.map((noteItem, index) => {
+                return (
+                    <Note
+                        key={index}
+                        id={index}
+                        title={noteItem.title}
+                        content={noteItem.content}
+                        onDelete={deleteNote}
+                        update={updateNote}
+                    />
+                );
+            })}
+            <Footer />
         </div>
-  );
+    );
 }
 
 export default App;
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,52 +1,39 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import DeleteIcon from "@material-ui/icons/Delete";
-import { TextareaAutosize, Input } from '@mui/material';
-
+import { TextareaAutosize, Input } from "@mui/material";
 
 function Note(props) {
-  const [note, setNote] = React.useState({
-    title: props.title,
-    content: props.content
-  });
+    useEffect(() => {}, [props]);
 
-  function handleClick() {
-    props.onDelete(props.id);
-  }
+    function handleClick() {
+        props.onDelete(props.id);
+    }
 
-  function update(event){
-    const { name, value } = event.target;
+    function update(event) {
+        const { name, value } = event.target;
+        props.update(props.id, name, value);
+    }
 
-    setNote((prevNote) => {
-      return {
-        ...prevNote,
-        [name]: value
-      };
-    });
-    props.update(note, props.id);
-  }
-
-  return (
-    <div className="note">
-        <Input
-          name="title"
-          onChange={update}
-          value = {note.title}
-          placeholder="Title"
-        />
-        <TextareaAutosize
-          placeholder="Content"
-          value = {note.content}
-          onChange={update}
-          name="content"
-        />
-        <button onClick={handleClick}>
-          <DeleteIcon />
-        </button>
-        <button>
-          Update Note
-        </button>
-    </div>
-  );
+    return (
+        <div className="note">
+            <Input
+                name="title"
+                onChange={update}
+                value={props.title}
+                placeholder="Title"
+            />
+            <TextareaAutosize
+                placeholder="Content"
+                value={props.content}
+                onChange={update}
+                name="content"
+            />
+            <button onClick={handleClick}>
+                <DeleteIcon />
+            </button>
+            <button>Update Note</button>
+        </div>
+    );
 }
 
 export default Note;
