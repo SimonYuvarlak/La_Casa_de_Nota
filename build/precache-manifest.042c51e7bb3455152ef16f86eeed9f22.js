8e357c7ca8d97a4e518fe99f79d24bd7cf0b95a9
Simon Arda Yuvarlak <simonyuvarlak@icloud.com>
2021-11-04 14:22:59 +0300

    delete link

---
+++ build/precache-manifest.042c51e7bb3455152ef16f86eeed9f22.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "216b2cc461bb501098811dee3ae94b26",
+    "url": "/La_Casa_de_Nota/index.html"
+  },
+  {
+    "revision": "d4a178f073ed24fc5208",
+    "url": "/La_Casa_de_Nota/static/js/2.0b565903.chunk.js"
+  },
+  {
+    "revision": "e9cd789265fcaea92670",
+    "url": "/La_Casa_de_Nota/static/js/main.9cacf755.chunk.js"
+  },
+  {
+    "revision": "085f67bcadd8a235678d",
+    "url": "/La_Casa_de_Nota/static/js/runtime-main.1a039abf.js"
+  },
+  {
+    "revision": "d33f5904f3d79578f61f7ced6e736fe7",
+    "url": "/La_Casa_de_Nota/static/media/dali_mask.d33f5904.svg"
+  }
+]);
