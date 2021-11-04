a686ddfae81dad6aa72dfc41efde34fa99f7fb87
Simon Arda Yuvarlak <simonyuvarlak@icloud.com>
2021-11-04 14:25:38 +0300

    Updates

---
+++ precache-manifest.d28cac3514d0ca21899a1c8e8dbf76ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db1f280bd95c8681d13ef5b9ac8d90f0",
+    "url": "/La_Casa_de_Nota/index.html"
+  },
+  {
+    "revision": "d4a178f073ed24fc5208",
+    "url": "/La_Casa_de_Nota/static/js/2.0b565903.chunk.js"
+  },
+  {
+    "revision": "e9cd789265fcaea92670",
+    "url": "/La_Casa_de_Nota/static/js/main.9cacf755.chunk.js"
+  },
+  {
+    "revision": "085f67bcadd8a235678d",
+    "url": "/La_Casa_de_Nota/static/js/runtime-main.1a039abf.js"
+  },
+  {
+    "revision": "d33f5904f3d79578f61f7ced6e736fe7",
+    "url": "/La_Casa_de_Nota/static/media/dali_mask.d33f5904.svg"
+  }
+]);
